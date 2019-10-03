3fc9bf52feed7535c7d2313abf640da6995a84a9
Thiyagarajah, Mayurathan <Mayurathan.Thiyagarajah@nav.no>
2019-10-03 11:59:56 +0200

    NOJIRA: Ny baseline

--- migreringer/src/main/resources/db/migration/defaultDS/1.0/V1.0_11__legg_inn_row_i_input_feed_for_infotrygd.sql
+++
@@ -1,2 +0,0 @@
-insert into INPUT_FEED (kode, navn, VENTETID_FERDIGLEST, VENTETID_LESBAR, VENTETID_FEILET) values
-('JF_INFOTRYGD', 'JSON feed fra Infotrygd', 'PT1H', 'PT1M', 'PT1M');
--- migreringer/src/main/resources/db/migration/defaultDS/3.0/V3.0_14__PFP-3478_fjerne_hendelse_payload_not_null.sql
+++
@@ -1,7 +0,0 @@
-alter table INNGAAENDE_HENDELSE modify (PAYLOAD null);
-update INNGAAENDE_HENDELSE a set a.PAYLOAD = null where a.SENDT_TID is null and a.SEKVENSNUMMER not in (
-  select b.KOBLING_ID from INNGAAENDE_HENDELSE b
-   where b.KOBLING_ID is not null
-     and b.ID != a.ID
-     and b.INPUT_FEED_KODE = a.INPUT_FEED_KODE
-     and b.SENDT_TID is not null);
