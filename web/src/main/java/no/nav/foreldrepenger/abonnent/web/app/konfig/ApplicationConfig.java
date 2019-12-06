686e1234e39d42cc47d4c062f4904f3645e09e38
Jan Erik Johnsen <jan.erik.johnsen@nav.no>
2019-12-06 15:48:27 +0100

    TFP-1898: Oppgradert til nyeste felles. Oppgradert til Swagger 2.1. Oppgradert diverse dependencies til nyeste versjon.

--- web/src/main/java/no/nav/foreldrepenger/abonnent/web/app/konfig/ApplicationConfig.java
+++ web/src/main/java/no/nav/foreldrepenger/abonnent/web/app/konfig/ApplicationConfig.java
@@ -3,16 +3,23 @@ package no.nav.foreldrepenger.abonnent.web.app.konfig;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-import io.swagger.jaxrs.config.BeanConfig;
+import io.swagger.v3.jaxrs2.integration.JaxrsOpenApiContextBuilder;
+import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
+import io.swagger.v3.oas.integration.OpenApiConfigurationException;
+import io.swagger.v3.oas.integration.SwaggerConfiguration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import no.nav.foreldrepenger.abonnent.web.app.exceptions.ConstraintViolationMapper;
 import no.nav.foreldrepenger.abonnent.web.app.exceptions.JsonMappingExceptionMapper;
 import no.nav.foreldrepenger.abonnent.web.app.exceptions.JsonParseExceptionMapper;
 import no.nav.vedtak.felles.prosesstask.rest.ProsessTaskRestTjeneste;
-import no.nav.vedtak.isso.config.ServerInfo;
 
 
 @ApplicationPath(ApplicationConfig.API_URI)
@@ -21,29 +28,37 @@ public class ApplicationConfig extends Application {
     public static final String API_URI = "/api";
 
     public ApplicationConfig() {
-        BeanConfig beanConfig = new BeanConfig();
-        beanConfig.setVersion("1.0");
-        if (ServerInfo.instance().isUsingTLS()) {
-            beanConfig.setSchemes(new String[]{"https"});
-        } else {
-            beanConfig.setSchemes(new String[]{"http"});
+        OpenAPI oas = new OpenAPI();
+        Info info = new Info()
+                .title("Vedtaksløsningen - Abonnent")
+                .version("1.0")
+                .description("REST grensesnitt for Fpabonnent.");
+
+        oas.info(info)
+                .addServersItem(new Server()
+                        .url("/fpabonnent"));
+        SwaggerConfiguration oasConfig = new SwaggerConfiguration()
+                .openAPI(oas)
+                .prettyPrint(true)
+                .scannerClass("io.swagger.v3.jaxrs2.integration.JaxrsAnnotationScanner")
+                .resourcePackages(Stream.of("no.nav")
+                        .collect(Collectors.toSet()));
+
+        try {
+            new JaxrsOpenApiContextBuilder<>()
+                    .openApiConfiguration(oasConfig)
+                    .buildContext(true)
+                    .read();
+        } catch (OpenApiConfigurationException e) {
+            throw new RuntimeException(e.getMessage(), e);
         }
-        
-        beanConfig.setBasePath("/fpabonnent" + API_URI);
-        beanConfig.setResourcePackage("no.nav");
-        beanConfig.setTitle("Vedtaksløsningen - Abonnent");
-        beanConfig.setDescription("REST grensesnitt for Vedtaksløsningen.");
-        beanConfig.setScan(true);
     }
 
     @Override
     public Set<Class<?>> getClasses() {
         Set<Class<?>> classes = new HashSet<>();
         classes.add(ProsessTaskRestTjeneste.class);
-
-        classes.add(io.swagger.jaxrs.listing.ApiListingResource.class);
-        classes.add(io.swagger.jaxrs.listing.SwaggerSerializers.class);
-
+        classes.add(OpenApiResource.class);
         classes.add(ConstraintViolationMapper.class);
         classes.add(JsonMappingExceptionMapper.class);
         classes.add(JsonParseExceptionMapper.class);
--- web/src/test/java/no/nav/foreldrepenger/abonnent/web/app/konfig/RestApiTester.java
+++ web/src/test/java/no/nav/foreldrepenger/abonnent/web/app/konfig/RestApiTester.java
@@ -11,11 +11,11 @@ import java.util.stream.Collectors;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Application;
 
-import io.swagger.jaxrs.listing.ApiListingResource;
+import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
 
 public class RestApiTester {
 
-    static final List<Class<?>> UNNTATT = Collections.singletonList(ApiListingResource.class);
+    static final List<Class<?>> UNNTATT = Collections.singletonList(OpenApiResource.class);
 
     static Collection<Method> finnAlleRestMetoder() {
         List<Method> liste = new ArrayList<>();
@@ -30,8 +30,6 @@ public class RestApiTester {
     }
 
     static Collection<Class<?>> finnAlleRestTjenester() {
-
-
         List<Class<?>> klasser = new ArrayList<>();
 
         klasser.addAll(finnAlleRestTjenester(new ApplicationConfig()));
