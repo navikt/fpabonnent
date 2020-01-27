30ec0ffdccb361a570247964b5b574ccaad12c62
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2020-01-27 16:36:56 +0100

    TZ

--- domene/src/test/java/no/nav/foreldrepenger/abonnent/tjenester/TpsFeedPollerOgInngåendeHendelseTest.java
+++ domene/src/test/java/no/nav/foreldrepenger/abonnent/tjenester/TpsFeedPollerOgInngåendeHendelseTest.java
@@ -13,6 +13,7 @@ import java.net.URI;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.TimeZone;
 import java.util.stream.Collectors;
 
 import org.junit.Before;
@@ -44,7 +45,9 @@ import no.nav.tjenester.person.feed.common.v1.Feed;
 import no.nav.vedtak.felles.integrasjon.rest.OidcRestClient;
 
 public class TpsFeedPollerOgInngåendeHendelseTest {
-
+    static {
+        TimeZone.setDefault(TimeZone.getTimeZone("Europe/Oslo"));
+    }
     private static final String BASE_URL_FEED = "http://foo.bar/tpsfeed";
     private URI startUri = URI.create(BASE_URL_FEED + "?sequenceId=1&pageSize=5");
     private URI endpoint = URI.create(BASE_URL_FEED);
@@ -71,9 +74,12 @@ public class TpsFeedPollerOgInngåendeHendelseTest {
         HendelseTjeneste fødselHendelseTjeneste = new FødselsmeldingOpprettetHendelseTjeneste();
         HendelseTjeneste dødHendelseTjeneste = new DødsmeldingOpprettetHendelseTjeneste();
         HendelseTjeneste dødfødselHendelseTjeneste = new DødfødselOpprettetHendelseTjeneste();
-        when(hendelseTjenesteProvider.finnTjeneste(eq(HendelseType.FØDSELSMELDINGOPPRETTET), anyLong())).thenReturn(fødselHendelseTjeneste);
-        when(hendelseTjenesteProvider.finnTjeneste(eq(HendelseType.DØDSMELDINGOPPRETTET), anyLong())).thenReturn(dødHendelseTjeneste);
-        when(hendelseTjenesteProvider.finnTjeneste(eq(HendelseType.DØDFØDSELOPPRETTET), anyLong())).thenReturn(dødfødselHendelseTjeneste);
+        when(hendelseTjenesteProvider.finnTjeneste(eq(HendelseType.FØDSELSMELDINGOPPRETTET), anyLong()))
+                .thenReturn(fødselHendelseTjeneste);
+        when(hendelseTjenesteProvider.finnTjeneste(eq(HendelseType.DØDSMELDINGOPPRETTET), anyLong()))
+                .thenReturn(dødHendelseTjeneste);
+        when(hendelseTjenesteProvider.finnTjeneste(eq(HendelseType.DØDFØDSELOPPRETTET), anyLong()))
+                .thenReturn(dødfødselHendelseTjeneste);
 
         poller = new TpsFeedPoller(endpoint, hendelseRepository, oidcRestClient, "5", "aktiv");
         inngåendeHendelseTjeneste = new InngåendeHendelseTjenesteImpl(hendelseRepository, hendelseTjenesteProvider);
@@ -82,7 +88,7 @@ public class TpsFeedPollerOgInngåendeHendelseTest {
 
     @Test
     public void skal_kunne_konvertere_json_fra_person_feed_til_hendelse() throws IOException {
-        //Last inn eksempel json og poll denne.
+        // Last inn eksempel json og poll denne.
         String eksempelJson = lesPayloadFraFil("/eksempel-gyldig-person-feed-v2.json");
         Feed feed = JsonMapper.fromJson(eksempelJson, Feed.class);
         assertThat(feed).isNotNull();
@@ -90,13 +96,19 @@ public class TpsFeedPollerOgInngåendeHendelseTest {
         poller.poll(inputFeed);
         repoRule.getEntityManager().flush();
 
-        //Sjekk at det har blitt opprettet hendelse for den ene fødselshendelsen som ligger i json fil.
+        // Sjekk at det har blitt opprettet hendelse for den ene fødselshendelsen som
+        // ligger i json fil.
         List<InngåendeHendelse> inngåendeHendelser = hendelseRepository.finnHendelserSomErKlareTilGrovsortering();
 
-        assertThat(inngåendeHendelser).hasSize(6); //Det er 1 FOEDSELSMELDINGOPPRETTET, 4 DOEDSMELDINGOPPRETTET og 1 DOEDFOEDSELOPPRETTET i eksempel json.
-        assertThat(inngåendeHendelser).flatExtracting(InngåendeHendelse::getSekvensnummer).contains(41L);//41 er hentet fra eksempel json.
-        assertThat(inngåendeHendelser).flatExtracting(InngåendeHendelse::getHåndtertStatus).containsOnly(HåndtertStatusType.MOTTATT);
-        List<HendelsePayload> hendelsePayloadListe = inngåendeHendelseTjeneste.getPayloadsForInngåendeHendelser(inngåendeHendelser);
+        assertThat(inngåendeHendelser).hasSize(6); // Det er 1 FOEDSELSMELDINGOPPRETTET, 4 DOEDSMELDINGOPPRETTET og 1
+                                                   // DOEDFOEDSELOPPRETTET i eksempel json.
+        assertThat(inngåendeHendelser).flatExtracting(InngåendeHendelse::getSekvensnummer).contains(41L);// 41 er hentet
+                                                                                                         // fra eksempel
+                                                                                                         // json.
+        assertThat(inngåendeHendelser).flatExtracting(InngåendeHendelse::getHåndtertStatus)
+                .containsOnly(HåndtertStatusType.MOTTATT);
+        List<HendelsePayload> hendelsePayloadListe = inngåendeHendelseTjeneste
+                .getPayloadsForInngåendeHendelser(inngåendeHendelser);
         assertThat(hendelsePayloadListe).hasSize(6);
 
         FødselHendelsePayload fødselHendelsePayload = (FødselHendelsePayload) hendelsePayloadListe.stream()
@@ -115,7 +127,8 @@ public class TpsFeedPollerOgInngåendeHendelseTest {
                 .map(h -> ((DødHendelsePayload) h).getAktørId().get())
                 .flatMap(java.util.Collection::stream)
                 .collect(Collectors.toSet());
-        assertThat(dødHendelseAktørIder).containsAll(asList("1155334466777", "7890123456789", "1234565432123", "1357911131723"));
+        assertThat(dødHendelseAktørIder)
+                .containsAll(asList("1155334466777", "7890123456789", "1234565432123", "1357911131723"));
 
         DødfødselHendelsePayload dødfødselHendelsePayload = (DødfødselHendelsePayload) hendelsePayloadListe.stream()
                 .filter(h -> h instanceof DødfødselHendelsePayload).findFirst().get();
@@ -126,7 +139,7 @@ public class TpsFeedPollerOgInngåendeHendelseTest {
 
     private String lesPayloadFraFil(String filnavn) throws IOException {
         try (InputStream resource = getClass().getResourceAsStream(filnavn);
-             Scanner scanner = new Scanner(resource, "UTF-8")) {
+                Scanner scanner = new Scanner(resource, "UTF-8")) {
             scanner.useDelimiter("\\A");
             return scanner.hasNext() ? scanner.next() : null;
         }
