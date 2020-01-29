a9f66d2cd45f96a76ecb78e678b1102ee3724df0
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2020-01-29 09:46:06 +0100

    cat

--- export-vault.sh
+++ export-vault.sh
@@ -1,33 +1,33 @@
 #!/usr/bin/env bash
 
-echo "Running  $0"
+echo "Kjører  $0"
 
 if test -f /config/oracle/jdbc_url;
 then
-   export  DEFAULTDS_URL=$(/config/oracle/jdbc_url)
-   echo "Setting DEFAULTDS_URL to $DEFAULTDS_URL"   
+   export  DEFAULTDS_URL=$(cat /config/oracle/jdbc_url)
+   echo "Setter DEFAULTDS_URL to $DEFAULTDS_URL"   
 fi
 
 if test -f /secrets/oracle/username;
 then
    export  DEFAULTDS_USERNAME= $(cat /secrets/oracle/username)
-   echo "Setting DEFAULTDS_USERNAME to $DEFAULTDS_USERNAME"   
+   echo "Setter DEFAULTDS_USERNAME to $DEFAULTDS_USERNAME"   
 fi
 
 if test -f /secrets/oracle/password;
 then
    export  DEFAULTDS_PASSWORD=$(cat /secrets/oracle/password)
-   echo "Setting DEFAULTDS_PASSWORD"   
+   echo "Setter DEFAULTDS_PASSWORD"   
 fi
 
 if test -f /secrets/serviceuser/fpabonnent/username;
 then
    export  SYSTEMBRUKER_USERNAME=$(cat /secrets/serviceuser/fpabonnent/username)
-   echo "Setting SYSTEMBRUKER_USERNAME to $SYSTEMBRUKER_USERNAME"   
+   echo "Setter SYSTEMBRUKER_USERNAME to $SYSTEMBRUKER_USERNAME"   
 fi
 
 if test -f /secrets/serviceuser/fpabonnent/password;
 then
    export  SYSTEMBRUKER_USERNAME=$(cat /secrets/serviceuser/fpabonnent/password)
-   echo "Setting SYSTEMBRUKER_PASSWORD"   
+   echo "Setter SYSTEMBRUKER_PASSWORD"   
 fi
