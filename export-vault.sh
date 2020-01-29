6120db7a5b8584b30a7b423675d90f83e5cd337e
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2020-01-29 09:18:39 +0100

    log variable settinger

--- export-vault.sh
+++ export-vault.sh
@@ -9,24 +9,24 @@ fi
 
 if test -f /secrets/oracle/username;
 then
-   export  DEFAULTDS_USERNAME=$(/secrets/oracle/username)
+   export  DEFAULTDS_USERNAME= $(cat /secrets/oracle/username)
    echo "Setting DEFAULTDS_USERNAME to $DEFAULTDS_USERNAME"   
 fi
 
 if test -f /secrets/oracle/password;
 then
-   export  DEFAULTDS_PASSWORD=$(/secrets/oracle/password)
+   export  DEFAULTDS_PASSWORD=$(cat /secrets/oracle/password)
    echo "Setting DEFAULTDS_PASSWORD"   
 fi
 
 if test -f /secrets/serviceuser/fpabonnent/username;
 then
-   export  SYSTEMBRUKER_USERNAME=$(/secrets/serviceuser/fpabonnent/username)
+   export  SYSTEMBRUKER_USERNAME=$(cat /secrets/serviceuser/fpabonnent/username)
    echo "Setting SYSTEMBRUKER_USERNAME to $SYSTEMBRUKER_USERNAME"   
 fi
 
 if test -f /secrets/serviceuser/fpabonnent/password;
 then
-   export  SYSTEMBRUKER_USERNAME=$(/secrets/serviceuser/fpabonnent/password)
+   export  SYSTEMBRUKER_USERNAME=$(cat /secrets/serviceuser/fpabonnent/password)
    echo "Setting SYSTEMBRUKER_PASSWORD"   
 fi
