aa2b3bf1431cf008c5bf75cc98fcf7c0fd90ea15
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2020-02-02 22:46:00 +0100

    FIX legg til p og fiks test (#107)

--- domene/src/test/java/no/nav/foreldrepenger/abonnent/feed/poller/TpsFeedPollerTest.java
+++ domene/src/test/java/no/nav/foreldrepenger/abonnent/feed/poller/TpsFeedPollerTest.java
@@ -52,7 +52,7 @@ public class TpsFeedPollerTest {
 
     @Before
     public void setUp() {
-        poller = new TpsFeedPoller(endpoint, hendelseRepository, oidcRestClient, "5", true);
+        poller = new TpsFeedPoller(endpoint, hendelseRepository, oidcRestClient, "5", "aktiv");
         Mockito.clearInvocations(oidcRestClient);
     }
 
@@ -110,7 +110,7 @@ public class TpsFeedPollerTest {
     @Test
     public void skal_ikke_polle_feed_når_deaktivert() {
         // Arrange
-        poller = new TpsFeedPoller(endpoint, hendelseRepository, oidcRestClient, "5", false);
+        poller = new TpsFeedPoller(endpoint, hendelseRepository, oidcRestClient, "5", "falsk");
 
         // Act
         poller.poll(inputFeed);
