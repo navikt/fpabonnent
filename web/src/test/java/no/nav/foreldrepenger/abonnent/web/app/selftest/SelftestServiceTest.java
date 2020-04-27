f18b3cee1a899101340eaa501890e106cb82840f
Frode Carlsen <frode.odde.carlsen@nav.no>
2020-04-27 08:35:04 +0200

    fix: bytt til getStringHeaders for forover-kompat (#167)

--- web/src/test/java/no/nav/foreldrepenger/abonnent/web/app/selftest/SelftestServiceTest.java
+++ web/src/test/java/no/nav/foreldrepenger/abonnent/web/app/selftest/SelftestServiceTest.java
@@ -92,8 +92,8 @@ public class SelftestServiceTest {
         Response response = service.doSelftest(TEXT_HTML, false);
 
         assertThat(response).isNotNull();
-        assertThat(response.getHeaders()).containsKeys("Content-Type");
-        assertThat(response.getHeaders().get("Content-Type")).contains(TEXT_HTML);
+        assertThat(response.getStringHeaders()).containsKeys("Content-Type");
+        assertThat(response.getStringHeaders().get("Content-Type")).contains(TEXT_HTML);
         assertThat(response.getEntity()).isNotNull();
     }
 
@@ -105,8 +105,8 @@ public class SelftestServiceTest {
         Response response = service.doSelftest(TEXT_HTML, true);
 
         assertThat(response).isNotNull();
-        assertThat(response.getHeaders()).containsKeys("Content-Type");
-        assertThat(response.getHeaders().get("Content-Type")).contains(TEXT_HTML);
+        assertThat(response.getStringHeaders()).containsKeys("Content-Type");
+        assertThat(response.getStringHeaders().get("Content-Type")).contains(TEXT_HTML);
         assertThat(response.getEntity()).isNotNull();
     }
 
