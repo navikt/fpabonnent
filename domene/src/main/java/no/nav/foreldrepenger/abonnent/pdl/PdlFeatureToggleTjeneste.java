b8c3b418ec15dee9aa537141c40b5e699bd7683c
Jan Erik Johnsen <jan.erik.johnsen@nav.no>
2020-05-28 12:13:46 +0200

    TFP-2847: Unleash fungerer ikke i Q på disse togglene - tester sjekk på cluster i stedet.

---
+++ domene/src/main/java/no/nav/foreldrepenger/abonnent/pdl/PdlFeatureToggleTjeneste.java
@@ -0,0 +1,26 @@
+package no.nav.foreldrepenger.abonnent.pdl;
+
+import java.util.Set;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import no.nav.vedtak.util.env.Cluster;
+import no.nav.vedtak.util.env.Environment;
+
+/**
+ * Alternativ til ustabil / ikke-fungerende Unleash...
+ */
+@ApplicationScoped
+public class PdlFeatureToggleTjeneste {
+
+    private static final Set<Cluster> SKAL_KONSUMERE_PDL_AKTIVERT = Set.of(Cluster.LOCAL, Cluster.DEV_FSS);
+    private static final Set<Cluster> SKAL_GROVSORTERE_PDL_AKTIVERT = Set.of(Cluster.LOCAL);
+
+    public boolean skalKonsumerePdl() {
+        return SKAL_KONSUMERE_PDL_AKTIVERT.contains(Environment.current().getCluster());
+    }
+
+    public boolean skalGrovsorterePdl() {
+        return SKAL_GROVSORTERE_PDL_AKTIVERT.contains(Environment.current().getCluster());
+    }
+}
