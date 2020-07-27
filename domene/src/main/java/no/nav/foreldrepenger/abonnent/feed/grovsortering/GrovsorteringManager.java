4443f7af4cfa6a7c57d56c2e7cb2baf0c6394efe
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2020-07-27 14:58:29 +0200

    NOJIRA oppgrader felles + prosesstask

--- domene/src/main/java/no/nav/foreldrepenger/abonnent/feed/grovsortering/GrovsorteringManager.java
+++ domene/src/main/java/no/nav/foreldrepenger/abonnent/feed/grovsortering/GrovsorteringManager.java
@@ -15,7 +15,6 @@ import org.slf4j.LoggerFactory;
 import no.nav.foreldrepenger.abonnent.felles.NamedThreadFactory;
 import no.nav.foreldrepenger.abonnent.konfig.KonfigVerdier;
 import no.nav.vedtak.apptjeneste.AppServiceHandler;
-import no.nav.vedtak.felles.jpa.VLPersistenceUnit;
 import no.nav.vedtak.konfig.KonfigVerdi;
 
 /**
@@ -42,7 +41,7 @@ public class GrovsorteringManager implements AppServiceHandler {
 
     @Inject
     public GrovsorteringManager(GrovsorteringVurderer grovsorteringVurderer,
-                                @VLPersistenceUnit EntityManager entityManager,
+                                EntityManager entityManager,
                                 @KonfigVerdi(value = SORTERING_INTERVALL_SEKUNDER_KEY, defaultVerdi = KonfigVerdier.SORTERING_INTERVALL_SEKUNDER_DEFAULT) Integer intervallSekunder) {
         this.grovsorteringVurderer = grovsorteringVurderer;
         this.entityManager = entityManager;
