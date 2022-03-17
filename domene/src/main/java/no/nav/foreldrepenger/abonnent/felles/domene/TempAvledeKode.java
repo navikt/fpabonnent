060554c381c1f9a7666016caecc32da019d800af
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2022-03-17 09:03:55 +0100

    Revert "Fjerne midlertidig håndtering av mellomlagret hendelse (#772)"
    This reverts commit e82a9d1116957993578636835985d9bd89c9324f.

---
+++ domene/src/main/java/no/nav/foreldrepenger/abonnent/felles/domene/TempAvledeKode.java
@@ -0,0 +1,60 @@
+package no.nav.foreldrepenger.abonnent.felles.domene;
+
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.TextNode;
+
+
+/**
+ * for avledning av kode for enum som ikke er mappet direkte på navn der både ny (@JsonValue) og gammel (@JsonProperty kode + kodeverk) kan
+ * bli sendt. Brukes til eksisterende kode er konvertert til @JsonValue på alle grensesnitt.
+ *
+ * <b>Gammel</b>: {"kode":"BT-004","kodeverk":"BEHANDLING_TYPE"}
+ * <p>
+ * <b>Ny</b>: "BT-004"
+ * <p>
+ *
+ * @deprecated endre grensesnitt til @JsonValue istdf @JsonProperty + @JsonCreator
+ */
+@Deprecated(since = "2020-09-17")
+public class TempAvledeKode {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TempAvledeKode.class);
+
+    @SuppressWarnings("rawtypes")
+    public static String getVerdi(Class<? extends Enum> enumCls, Object node, String key) {
+        String kode;
+        if (node instanceof String) {
+            kode = (String) node;
+        } else {
+            if (node instanceof JsonNode) {
+                kode = ((JsonNode) node).get(key).asText();
+            } else if (node instanceof TextNode) {
+                kode = ((TextNode) node).asText();
+            } else if (node instanceof Map) {
+                kode = (String) ((Map) node).get(key);
+            } else {
+                throw new IllegalArgumentException("Støtter ikke node av type: " + node.getClass() + " for enum:" + enumCls.getName());
+            }
+            String kodeverk = "uspesifisert";
+            try {
+                if (node instanceof JsonNode) {
+                    kodeverk = ((JsonNode) node).get("kodeverk").asText();
+                } else if (node instanceof TextNode) {
+                    kodeverk = ((TextNode) node).asText();
+                } else if (node instanceof Map) {
+                    kodeverk = (String) ((Map) node).get("kodeverk");
+                }
+            } catch (Exception e) {
+                LOG.info("KODEVERK-OBJEKT: tempavledekode kalt uten at det finnes kodeverk - kode {}", kode);
+            }
+            LOG.info("KODEVERK-OBJEKT: mottok kodeverdiobjekt som ikke var String - kode {} fra kodeverk {} ", kode, kodeverk);
+        }
+        return kode;
+    }
+
+}
