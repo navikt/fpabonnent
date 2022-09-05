a3571ba1b1fe32ad797a9b95d6b4e88b2bdf719c
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2022-09-05 22:19:15 +0200

    Forenkling av abac, contract-fase

--- web/src/main/java/no/nav/foreldrepenger/abonnent/web/server/abac/AppPdpRequestBuilderImpl.java
+++ web/src/main/java/no/nav/foreldrepenger/abonnent/web/server/abac/AppPdpRequestBuilderImpl.java
@@ -2,11 +2,7 @@ package no.nav.foreldrepenger.abonnent.web.server.abac;
 
 import javax.enterprise.context.ApplicationScoped;
 
-import no.nav.vedtak.sikkerhet.abac.AbacAttributtSamling;
 import no.nav.vedtak.sikkerhet.abac.AbacDataAttributter;
-import no.nav.vedtak.sikkerhet.abac.NavAbacCommonAttributter;
-import no.nav.vedtak.sikkerhet.abac.PdpKlient;
-import no.nav.vedtak.sikkerhet.abac.PdpRequest;
 import no.nav.vedtak.sikkerhet.abac.PdpRequestBuilder;
 import no.nav.vedtak.sikkerhet.abac.pdp.AppRessursData;
 
@@ -15,23 +11,6 @@ import no.nav.vedtak.sikkerhet.abac.pdp.AppRessursData;
  */
 @ApplicationScoped
 public class AppPdpRequestBuilderImpl implements PdpRequestBuilder {
-
-    private static final String ABAC_DOMAIN = "foreldrepenger";
-
-    @Override
-    public PdpRequest lagPdpRequest(AbacAttributtSamling attributter) {
-        PdpRequest pdpRequest = new PdpRequest();
-        pdpRequest.put(PdpKlient.ENVIRONMENT_AUTH_TOKEN, attributter.getIdToken());
-        pdpRequest.put(NavAbacCommonAttributter.RESOURCE_FELLES_DOMENE, ABAC_DOMAIN);
-        pdpRequest.put(NavAbacCommonAttributter.XACML10_ACTION_ACTION_ID, attributter.getActionType().getEksternKode());
-        pdpRequest.put(NavAbacCommonAttributter.RESOURCE_FELLES_RESOURCE_TYPE, attributter.getResource());
-        return pdpRequest;
-    }
-
-    @Override
-    public boolean nyttAbacGrensesnitt() {
-        return true;
-    }
 
     @Override
     public AppRessursData lagAppRessursData(AbacDataAttributter dataAttributter) {
