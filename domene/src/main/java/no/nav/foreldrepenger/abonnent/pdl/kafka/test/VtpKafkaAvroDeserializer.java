b209f4ca4a969a0a9c0ce82d658c7f2d8aaa93b3
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2023-06-14 23:54:50 +0200

    Confluent 7.4 med utvidet VTP-mock-schemareg

--- domene/src/main/java/no/nav/foreldrepenger/abonnent/pdl/kafka/test/VtpKafkaAvroDeserializer.java
+++ domene/src/main/java/no/nav/foreldrepenger/abonnent/pdl/kafka/test/VtpKafkaAvroDeserializer.java
@@ -1,6 +1,7 @@
 package no.nav.foreldrepenger.abonnent.pdl.kafka.test;
 
 import org.apache.avro.Schema;
+import org.apache.kafka.common.header.Headers;
 
 import io.confluent.kafka.schemaregistry.avro.AvroSchema;
 import io.confluent.kafka.schemaregistry.client.MockSchemaRegistryClient;
@@ -14,6 +15,12 @@ public class VtpKafkaAvroDeserializer extends KafkaAvroDeserializer {
     public Object deserialize(String topic, byte[] bytes) {
         this.schemaRegistry = getMockClient(Personhendelse.SCHEMA$);
         return super.deserialize(topic, bytes);
+    }
+
+    @Override
+    public Object deserialize(String topic, Headers headers, byte[] bytes) {
+        this.schemaRegistry = getMockClient(Personhendelse.SCHEMA$);
+        return super.deserialize(topic, headers, bytes);
     }
 
     private static SchemaRegistryClient getMockClient(final Schema schema) {
