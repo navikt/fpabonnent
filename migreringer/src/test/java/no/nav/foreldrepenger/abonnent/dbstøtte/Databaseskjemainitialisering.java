8fb9437944a4045c6281b74d16d9ff78cdef9e94
Anders Palfi <anderspalfi@gmail.com>
2024-04-18 10:19:30 +0200

    fjerner flyway clean on validation error for tester ettersom det ikke er støttet mot oracle (#1164)

--- migreringer/src/test/java/no/nav/foreldrepenger/abonnent/dbstøtte/Databaseskjemainitialisering.java
+++ migreringer/src/test/java/no/nav/foreldrepenger/abonnent/dbstøtte/Databaseskjemainitialisering.java
@@ -2,6 +2,7 @@ package no.nav.foreldrepenger.abonnent.dbstøtte;
 
 import static java.lang.Runtime.getRuntime;
 
+import java.sql.SQLException;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import javax.naming.NamingException;
@@ -48,21 +49,18 @@ public final class Databaseskjemainitialisering {
                 .locations(DB_SCRIPT_LOCATION + SCHEMA)
                 .table("schema_version")
                 .baselineOnMigrate(true)
-                .cleanDisabled(false)
                 .load();
             try {
                 if (!ENV.isLocal()) {
                     throw new IllegalStateException("Forventer at denne migreringen bare kjøres lokalt");
                 }
                 flyway.migrate();
-            } catch (FlywayException fwe) {
-                try {
-                    // prøver igjen
-                    flyway.clean();
-                    flyway.migrate();
-                } catch (FlywayException fwe2) {
-                    throw new IllegalStateException("Migrering feiler", fwe2);
+                var connection = flyway.getConfiguration().getDataSource().getConnection();
+                if (!connection.isClosed()) {
+                    connection.close();
                 }
+            } catch (SQLException sqlex) {
+                // nothing to do here
             }
         }
     }
