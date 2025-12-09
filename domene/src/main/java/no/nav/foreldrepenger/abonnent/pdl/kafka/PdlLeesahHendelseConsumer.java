2b89b6f84fddee875f012c75e998137b119af5ce
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2025-12-09 19:38:15 +0100

    TFP-5694 slutt å komsumere Leesah

--- domene/src/main/java/no/nav/foreldrepenger/abonnent/pdl/kafka/PdlLeesahHendelseConsumer.java
+++ domene/src/main/java/no/nav/foreldrepenger/abonnent/pdl/kafka/PdlLeesahHendelseConsumer.java
@@ -10,42 +10,42 @@ import no.nav.vedtak.felles.integrasjon.kafka.KafkaConsumerManager;
 import no.nav.vedtak.server.Controllable;
 import no.nav.vedtak.server.LiveAndReadinessAware;
 
-@ApplicationScoped
-public class PdlLeesahHendelseConsumer implements LiveAndReadinessAware, Controllable {
-
-    private static final Logger LOG = LoggerFactory.getLogger(PdlLeesahHendelseConsumer.class);
-
-    private KafkaConsumerManager<String, Personhendelse> kcm;
-
-    PdlLeesahHendelseConsumer() {
-    }
-
-    @Inject
-    public PdlLeesahHendelseConsumer(
-        PdlLeesahHendelseHåndterer håndterer) {
-        this.kcm = new KafkaConsumerManager<>(håndterer);
-    }
-
-    @Override
-    public boolean isAlive() {
-        return kcm.allRunning();
-    }
-
-    @Override
-    public boolean isReady() {
-        return isAlive();
-    }
-
-    @Override
-    public void start() {
-        LOG.info("Starter konsumering av topics={}", kcm.topicNames());
-        kcm.start((t, e) -> LOG.error("{} :: Caught exception in stream, exiting", t, e));
-    }
-
-    @Override
-    public void stop() {
-        LOG.info("Starter shutdown av topics={} med 10 sekunder timeout", kcm.topicNames());
-        kcm.stop();
-    }
-
-}
+//@ApplicationScoped
+//public class PdlLeesahHendelseConsumer implements LiveAndReadinessAware, Controllable {
+//
+//    private static final Logger LOG = LoggerFactory.getLogger(PdlLeesahHendelseConsumer.class);
+//
+//    private KafkaConsumerManager<String, Personhendelse> kcm;
+//
+//    PdlLeesahHendelseConsumer() {
+//    }
+//
+//    @Inject
+//    public PdlLeesahHendelseConsumer(
+//        PdlLeesahHendelseHåndterer håndterer) {
+//        this.kcm = new KafkaConsumerManager<>(håndterer);
+//    }
+//
+//    @Override
+//    public boolean isAlive() {
+//        return kcm.allRunning();
+//    }
+//
+//    @Override
+//    public boolean isReady() {
+//        return isAlive();
+//    }
+//
+//    @Override
+//    public void start() {
+//        LOG.info("Starter konsumering av topics={}", kcm.topicNames());
+//        kcm.start((t, e) -> LOG.error("{} :: Caught exception in stream, exiting", t, e));
+//    }
+//
+//    @Override
+//    public void stop() {
+//        LOG.info("Starter shutdown av topics={} med 10 sekunder timeout", kcm.topicNames());
+//        kcm.stop();
+//    }
+//
+//}
